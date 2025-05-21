f2afe0b1d157f73fe18d2610c7163321c851ae4f
Jason Bao <bao.jason.z@gmail.com>
2025-05-21 11:15:19 -0400

    Terraform state dynamo tables can't have vars

--- global/main.tf
+++ global/main.tf
@@ -2,16 +2,45 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "{insert_bucket_name}"
+    key            = "tf/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "{insert_table_name}"
+  }
+}
+
 resource "aws_s3_bucket" "terraform_prod" {
   bucket = "insert globally unique bucket name"
+}
+
+resource "aws_dynamodb_table" "terraform-state-prod" {
+  name           = "terraform-state-prod"
+  read_capacity  = 1
+  write_capacity = 1
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 resource "aws_s3_bucket" "terraform_dev" {
   bucket = "insert globally unique bucket name"
 }
 
-resource "aws_s3_bucket" "terraform_global" {
-  bucket = "insert globally unique bucket name"
+resource "aws_dynamodb_table" "terraform-state-dev" {
+  name           = "terraform-state-dev"
+  read_capacity  = 1
+  write_capacity = 1
+
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 module "global" {
--- prod/state.tf
+++ prod/state.tf
@@ -3,18 +3,6 @@ terraform {
     bucket         = data.terraform_remote_state.global.outputs.prod_terraform_bucket_name
     key            = "tf/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-state-prod"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform-state-prod" {
-  name           = "terraform-state-prod"
-  read_capacity  = 1
-  write_capacity = 1
-
-  hash_key = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
+    dynamodb_table = data.terraform_remote_state.global.outputs.prod_terrafrom_dynamo_table_name
   }
 }
