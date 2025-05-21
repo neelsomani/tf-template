ef7ded50816471c8f61a3e330bdb49d90d1583c2
Jason Bao <bao.jason.z@gmail.com>
2025-05-21 16:45:14 -0400

    Cannot use data in backend block

--- prod/state.tf
+++ prod/state.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = data.terraform_remote_state.global.outputs.prod_terraform_bucket_name
+    bucket         = "<insert terraform bucket name>"
     key            = "tf/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = data.terraform_remote_state.global.outputs.prod_terrafrom_dynamo_table_name
+    dynamodb_table = "terraform-state-prod"
   }
 }
